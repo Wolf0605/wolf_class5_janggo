03e314b776992eb5c8584672fc1af6da87982df6
Wolf0605 <mmd0a605@naver.com>
2021-07-01 10:31:32 +0900

    2021/07/01 calss 5 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def hello_world(requests):
-    return HttpResponse('Hello World!')
+def hello_world(request):
+    return render(request, 'accountapp/hello_world.html')
