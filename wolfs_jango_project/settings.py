d0614e0d600be890f913ebbf75de4dd69ff61b3a
Wolf0605 <mmd0a605@naver.com>
2021-07-05 10:46:10 +0900

    2021/07/5 calss 5 commit 2

--- wolfs_jango_project/settings.py
+++ wolfs_jango_project/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]# Base_dir 은 최상위 폴더
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
