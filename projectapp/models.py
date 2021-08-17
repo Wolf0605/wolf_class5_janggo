a7da09eca6634c08f55cdc31ac49de86fbfd2844
Wolf0605 <mmd0a605@naver.com>
2021-08-17 14:42:38 +0900

    2021/08/17 calss 5 commit 1

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=True) #DB에서 null처리가 된다
+    image = models.ImageField(upload_to='project/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
