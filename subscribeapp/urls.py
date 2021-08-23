32d9c461105f6f06150dac371a7ba172678ef0ab
Wolf0605 <mmd0a605@naver.com>
2021-08-23 10:48:38 +0900

    2021/08/23 calss 5 commit 5

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -5,5 +5,6 @@ from subscribeapp.views import SubscriptionView
 app_name = 'subscribeapp'
 urlpatterns =  [
     path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubscriptionView.as_view(),name ='list'),
 
 ]
