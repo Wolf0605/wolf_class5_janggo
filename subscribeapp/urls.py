816652f62f263ec5cfda55efc453c0962f2bcf64
Wolf0605 <mmd0a605@naver.com>
2021-08-23 09:43:36 +0900

    2021/08/23 calss 5 commit 2

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,4 +1,9 @@
-urlpatterns =  [
+from django.urls import path
 
+from subscribeapp.views import SubscriptionView
+
+app_name = 'subscribeapp'
+urlpatterns =  [
+    path('subscribe/<int:pk>', SubscriptionView.as_view(), name='subscribe'),
 
 ]
