0608555132f9885235642fd66027b3ead0473fc5
Wolf0605 <mmd0a605@naver.com>
2021-08-26 09:25:38 +0900

    2021/08/26 calss 5 commit 1

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.context_processors import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -19,11 +20,15 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user,
                                                 article=article)
         if like_record.exists():
+            # 좋아요가 반영 X
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다') # 이 메세지가 ERROR메시지다
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
         article.like += 1
         article.save()
+        # 좋아요가 반영 0
+        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다')
 
         return super().get(request, *args, **kwargs)
     def get_redirect_url(self, *args, **kwargs):
