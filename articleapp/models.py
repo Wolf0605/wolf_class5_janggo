193d8e1c7829caedf6b5fa03436ec1c01ccfa815
Wolf0605 <mmd0a605@naver.com>
2021-08-31 09:41:15 +0900

    2021/08/31 calss 5 commit 4

--- articleapp/models.py
+++ articleapp/models.py
@@ -12,7 +12,7 @@ class Article(models.Model):
     project = models.ForeignKey(Project,
                                 on_delete=models.SET_NULL,
                                 related_name='article',
-                                null=True)  #models.SET회원이 삭제된다고 글이 삭제되진않음
+                                null=True, blank=True)  #models.SET회원이 삭제된다고 글이 삭제되진않음
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='articles/', null=True)
     content = models.TextField(null=True)
