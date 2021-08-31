b3b25314445746927b32e73e6b7550023461628a
Wolf0605 <mmd0a605@naver.com>
2021-08-31 09:30:12 +0900

    2021/08/31 calss 5 commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,19 +16,6 @@ from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 
-@login_required(login_url=reverse_lazy('accountapp:login'))     # 장고 기본 기능,, accounts/login이 자동 경로로 저장
-def hello_world(request):
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-            return HttpResponseRedirect(reverse('accountapp:hello_world')) # reverse 로 주소를 역 추적.
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html',
-                          context={'hello_world_list': hello_world_list})
-
 has_ownership = [login_required(login_url=reverse_lazy('accountapp:login')), account_ownership_required]
 
 @method_decorator(has_ownership, 'get')
@@ -42,7 +29,7 @@ class AccountUpdateView(UpdateView):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world') # class에서 rever.lazy()
+    success_url = reverse_lazy('articleapp:List') # class에서 rever.lazy()
     template_name = 'accountapp/create.html'
 
 
@@ -51,7 +38,7 @@ class AccountCreateView(CreateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:List')
     template_name = 'accountapp/delete.html'
 
 
