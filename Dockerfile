98ad40fd8f8afe9d6c9cd94fb1f72238108f0b93
Wolf0605 <mmd0a605@naver.com>
2021-09-14 15:54:07 +0900

    2021/09/14 calss 5 commit 1

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,25 @@
 FROM python:3.9.0
 
 WORKDIR /home/
+
+RUN echo 'wolfowlfowlf'
 # home 이라는 경로에 모든 git 파일 붙혀넣음
 RUN git clone https://github.com/Wolf0605/wolf_class5_janggo.git
 
 WORKDIR /home/wolf_class5_janggo/
 
-RUN pip install -r requirements.txt
 # 개발하는 한경과 저 환경을 매칭
 
+RUN echo "SECRET_KEY=django-insecure-62cxbw+*g^rmt%_6a0eh6i1kvkd1sw2_sc^@*q1u$tlloo=*@$"> .env
+
+RUN pip install -r requirements.txt
+
+RUN pip install gunicorn
+
 RUN python manage.py migrate
+
+RUN python manage.py collectstatic
+
+EXPOSE 8000
+
+CMD ["gunicorn", "wolfs_jango_project.wsgi", "--bind", "0.0.0.0:8000"]
--- wolfs_jango_project/settings/base.py
+++ wolfs_jango_project/settings/base.py
@@ -15,34 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-local_env = open(os.path.join(BASE_DIR, '.env'))
-
-env_list = dict()
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"] #모든호스트의 접속을 허가 한다
 
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
@@ -101,16 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wolfs_jango_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
