03b9e05f3def65e6d1ca563c911a3445be37e3bf
Wolf0605 <mmd0a605@naver.com>
2021-09-28 15:20:32 +0900

    BUGFIX: 2021/09/28 calss 5 commit 2

--- wolfs_jango_project/settings/deploy.py
+++ wolfs_jango_project/settings/deploy.py
@@ -1,24 +1,14 @@
 from .base import *
 
-local_env = open(os.path.join(BASE_DIR, '.env'))
-
-env_list = dict()
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secrret = secret.lstrip().rstrip()
+    file.close()
+    return secret
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -34,8 +24,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306', # 여기가 문제였네요 상상도 못한 정체 포트 3306입니다 5432 아니구요 ㅠ
     }
